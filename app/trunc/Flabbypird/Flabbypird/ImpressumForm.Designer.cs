d3a53f79c7dba0dc30a4f244c80a310637ce9628
ESeer <e.seer@web.de>
2014-07-09 15:04:02 +0200

    Impressum

---
+++ app/trunc/Flabbypird/Flabbypird/ImpressumForm.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class ImpressumForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ImpressumForm));
+            this.Impressum = new System.Windows.Forms.Label();
+            this.logo = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Impressum
+            // 
+            this.Impressum.AutoSize = true;
+            this.Impressum.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F);
+            this.Impressum.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.Impressum.Location = new System.Drawing.Point(53, 52);
+            this.Impressum.Name = "Impressum";
+            this.Impressum.Size = new System.Drawing.Size(72, 36);
+            this.Impressum.TabIndex = 0;
+            this.Impressum.Text = "Text";
+            // 
+            // logo
+            // 
+            this.logo.Image = ((System.Drawing.Image)(resources.GetObject("logo.Image")));
+            this.logo.Location = new System.Drawing.Point(185, 297);
+            this.logo.Name = "logo";
+            this.logo.Size = new System.Drawing.Size(379, 170);
+            this.logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.logo.TabIndex = 1;
+            this.logo.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(637, 530);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 20);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Zurück";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ImpressumForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.logo);
+            this.Controls.Add(this.Impressum);
+            this.Name = "ImpressumForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Impressum;
+        private System.Windows.Forms.PictureBox logo;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ app/trunc/Flabbypird/Flabbypird/ImpressumForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class ImpressumForm : Form
+    {
+        public ImpressumForm(Point position)
+        {
+            InitializeComponent();
+
+            this.Text = "Impressum";
+
+            this.StartPosition = FormStartPosition.Manual;
+
+            this.Size = new Size() { Width = 800, Height = 600 };
+            this.FormBorderStyle = FormBorderStyle.Fixed3D;
+
+            Impressum.Text = "Digital-Torpedo " + Environment.NewLine + 
+              "Ekhard Seer e.seer@web.de" + Environment.NewLine + 
+"Jakob Warmhold ***keine angaben***" + Environment.NewLine +
+"Felix Martin f.martin.1995@web.de" + Environment.NewLine + 
+"Theodor Gaede TheodorG@outlook.com";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
