f4d629ebe2160b54a9084f9df13757ee6fdf109c
Jakob Warmhold <jakob@warmhold.de>
2014-07-10 11:04:53 +0200

    fixes #11  Write Methode angepasst. Konsistenz-Check-Methode wird von einer anderen Klasse geregelt werden.

--- app/trunc/Flabbypird/Flabbypird/GameConfig.cs
+++ app/trunc/Flabbypird/Flabbypird/GameConfig.cs
@@ -6,6 +6,7 @@ using System.Xml;
 using System.Data;
 using System.Data.OleDb;
 using System.Collections;
+using System.IO;
 
 namespace Flabbypird
 {
@@ -14,9 +15,10 @@ namespace Flabbypird
         /// <summary>
         /// In case the config is damaged use this method to reset config to default
         /// </summary>
+        public static string path = Environment.CurrentDirectory;
         public static void New()
         {
-            XmlTextWriter writer = new XmlTextWriter(@"C:\\Users\warmhold\test.xml", null);  //inititalize writer Variable with XML-Docuent 
+            XmlTextWriter writer = new XmlTextWriter(Path.Combine(path, "config.xml"), null);  //inititalize writer Variable with XML-Docuent 
             writer.WriteStartDocument();  // start parameter ==> Start Writitng Document
             writer.WriteStartElement("Settings"); //Define first Element called "Settings"
             writer.WriteStartElement("Volume"); //Define Child Element called "Volume". Parent: Element named "Settings" 
@@ -42,7 +44,7 @@ namespace Flabbypird
             Hashtable settings = new Hashtable();
 
             XmlDocument xmlDoc = new XmlDocument();
-            xmlDoc.Load(@"C:\\Users\warmhold\test.xml");
+            xmlDoc.Load(Path.Combine(path, "config.xml"));
             XmlNode volume = xmlDoc.SelectSingleNode("//Settings/Volume");
             XmlAttribute valueVolume = volume.Attributes["Value"];
             XmlAttribute enabledVolume = volume.Attributes["Enabled"];
@@ -61,43 +63,19 @@ namespace Flabbypird
             return settings;
         }
         /// <summary>
-        /// Write the config. In case you want to use default part just send an empty string (language) or a specific value(see parameter informatons).
-        /// Example: GameConfig.GameConfig.Write(-1,true,600,800,"");
+        /// Write to config. 
         /// </summary>
-        /// <param name="volumeValue">int. Set your Volume here. Anything smaller than 0 and bigger than 100 will use default value.</param>
-        /// <param name="volumeEnabled">bool. Volume enabled or not ? Nothing else to explain here I hope.</param>
-        /// <param name="resolutionHeight">int. Resolution height. Won't change anything. Changing resolution is not implemented yet.</param>
-        /// <param name="resolutionWidth">int. Resolution width. Won't change anything. Changing resolution is not implemented yet.</param>
-        /// <param name="languageValue">string. Language. At the moment we only support german. So here we have a useles variable again you have to set :P</param>
-        public static void Write(int volumeValue, bool volumeEnabled, int resolutionHeight, int resolutionWidth, string languageValue)
+        /// <param name="parentattribute">Main attribute you want to edit. Example: "Volume"</param>
+        /// <param name="attribute">Attribute you want to change from main attribute. Example: "Value"</param>
+        /// <param name="value">Value you want to set to the attribute. Example: "75"</param>
+        public static void test(string parentattribute, string attribute, string value)
         {
             XmlDocument xmlDoc = new XmlDocument();
-            xmlDoc.Load(@"C:\\Users\warmhold\test.xml");
-            XmlNode volume = xmlDoc.SelectSingleNode("//Settings/Volume");
-            XmlAttribute valueVolume = volume.Attributes["Value"];
-            XmlAttribute enabledVolume = volume.Attributes["Enabled"];
-            XmlNode lang = xmlDoc.SelectSingleNode("//Settings/Language");
-            XmlAttribute valueLang = lang.Attributes["Value"];
-            XmlNode resolution = xmlDoc.SelectSingleNode("//Settings/Resolution");
-            XmlAttribute height = resolution.Attributes["Height"];
-            XmlAttribute width = resolution.Attributes["Width"];
-            if (volumeValue >= 0 && volumeValue <= 100)
-            {
-                valueVolume.Value = Convert.ToString(volumeValue);
-            }
-                enabledVolume.Value = volumeEnabled.ToString();
-            if (languageValue == "DE")
-            {
-            valueLang.Value = languageValue;
-            }
-            if (resolutionHeight == 600) // Changing resolution not implemented yet.
-            {
-            height.Value = Convert.ToString(resolutionHeight);
-            }
-            if (resolutionWidth == 800) // Changing resolution not implemented yet.
-            {
-            width.Value = Convert.ToString(resolutionWidth);
-            }
+            xmlDoc.Load(Path.Combine(path, "config.xml"));
+            XmlNode volume = xmlDoc.SelectSingleNode("//Settings/" + parentattribute);
+            XmlAttribute attributes = volume.Attributes[attribute];
+            attributes.Value = value;
+            xmlDoc.Save(Path.Combine(path, "test.xml"));
         }
     }
 }
