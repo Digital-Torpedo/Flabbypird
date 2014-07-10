cfe970e2f1b41371db37a79861abd406aa025fc0
ESeer <e.seer@web.de>
2014-07-10 14:30:07 +0200

    Hilfe geadded

---
+++ app/trunc/Flabbypird/Flabbypird/Hilfe.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Hilfe
+{
+    partial class Hilfe
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Hilfe));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.control = new System.Windows.Forms.Button();
+            this.goal = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(568, 518);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Zurück";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.back_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F);
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(52, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 36);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(233, 169);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(259, 256);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // control
+            // 
+            this.control.Location = new System.Drawing.Point(187, 518);
+            this.control.Name = "control";
+            this.control.Size = new System.Drawing.Size(169, 23);
+            this.control.TabIndex = 3;
+            this.control.Text = "Steuerung";
+            this.control.UseVisualStyleBackColor = true;
+            this.control.Click += new System.EventHandler(this.control_Click);
+            // 
+            // goal
+            // 
+            this.goal.Location = new System.Drawing.Point(12, 518);
+            this.goal.Name = "goal";
+            this.goal.Size = new System.Drawing.Size(169, 23);
+            this.goal.TabIndex = 4;
+            this.goal.Text = "Ziel des Spiels";
+            this.goal.UseVisualStyleBackColor = true;
+            this.goal.Click += new System.EventHandler(this.goal_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.goal);
+            this.Controls.Add(this.control);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button control;
+        private System.Windows.Forms.Button goal;
+    }
+}
+
---
+++ app/trunc/Flabbypird/Flabbypird/Hilfe.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Hilfe
+{
+    public partial class Hilfe : Form
+    {
+        /// <summary>
+        /// Stellt die Form da und gibt den "Ziel des Spiels" Text aus "
+        /// </summary>
+        public Hilfe()
+        {
+            InitializeComponent();
+            this.Text = "Hilfe";
+            this.Size = new Size()
+            {
+                Width = 800,
+                Height = 600
+            };
+            this.FormBorderStyle = FormBorderStyle.Fixed3D;
+            this.Location = new Point();
+            pictureBox1.Hide();
+            label1.Text = "Das Ziel des Spiels ist es soweit wie möglich"+Environment.NewLine+
+                          "im Level voranzukommen ohne mit der"+Environment.NewLine+
+                          "Spielumgebung in Kontakt zu kommen";
+        }
+
+
+        /// <summary>
+        ///  Event das ausgeführt wird wenn der "Zurück" Button gedrückt wird
+        /// </summary>
+        /// <param name="sender">sender objekt</param>
+        /// <param name="e">event argument</param>
+        private void back_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// Event das ausgeführt wird wenn der "Ziel des Spiels" Button gedrückt wird
+        /// </summary>
+        /// <param name="sender">sender objekt</param>
+        /// <param name="e">event argument</param>
+        private void goal_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Hide();
+            label1.Text = "Das Ziel des Spiels ist es soweit wie möglich" + Environment.NewLine +
+                          "im Level voranzukommen ohne mit der" + Environment.NewLine +
+                          "Spielumgebung in Kontakt zu kommen";
+        }
+
+        /// <summary>
+        /// Event das ausgeführt wird wenn der "Steuerung" Button gedrückt wird
+        /// </summary>
+        /// <param name="sender">sender objekt</param>
+        /// <param name="e">event argument</param>
+        private void control_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Show();
+            label1.Text = "Drücken Sie wiederholt die Leertaste um zu flattern.";
+        }
+    }
+}
